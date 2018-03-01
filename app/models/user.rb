29522548c72fb72cefdc5b040115c483d3e1cc76
Reid South <rbsouth@gmail.com>
2018-02-28 18:17:36 -0700

    user has default and can add profile picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,13 @@ class User < ApplicationRecord
 	has_many :posts, dependent: :destroy
 	has_many :comments, dependent: :destroy
 
+	before_save :set_default_image
+	def set_default_image
+		if self.profile_picture.nil?
+			self.profile_picture = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoHJ8UPczwkdjEgvCYSQ4TKRsCe4orkTg35k1vNLKlSIknhPcV"
+		end
+	end
+
 	validates :email, presence: true, uniqueness: true
 	validates :first_name, presence: true
 	validates :last_name, presence: true
